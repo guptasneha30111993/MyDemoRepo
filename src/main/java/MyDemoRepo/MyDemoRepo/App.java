d87c356f690a3c230a684f767007a57fb80f758a
Sneha Gupta2 <sneha.gupta2@synapse.com>
2019-04-01 13:58:34 +0530

    first commit

---
+++ src/main/java/MyDemoRepo/MyDemoRepo/App.java
@@ -0,0 +1,13 @@
+package MyDemoRepo.MyDemoRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
